ca9db6d825d51fb3674c4ec3b93fb0de22a90789
lucaslol <61584821+lucasyee999@users.noreply.github.com>
2024-09-02 23:39:27 +0800

    added themes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+
+    { "ellisonleao/gruvbox.nvim" },
+  
+    -- currently added themes
+    -- 1) catppuccin (very nice)
+    -- 2) gruvbox (idk never use before)
+    -- 3) quiet (luffy theme black n white)
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "quiet",
+      },
+    }
+  }
