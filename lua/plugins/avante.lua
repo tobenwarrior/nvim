011cb4e4ffbb479464993dd2447098c0bf153238
lucaslol <61584821+lucasyee999@users.noreply.github.com>
2024-09-04 21:31:21 +0800

    go

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,20 +2,20 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
+  version = false, -- set this if you want to always pull the latest change
   opts = {
     -- add any opts here
   },
-  keys = {
-    { "<leader>aa", function() require("avante.api").ask() end, desc = "avante: ask", mode = { "n", "v" } },
-    { "<leader>ar", function() require("avante.api").refresh() end, desc = "avante: refresh" },
-    { "<leader>ae", function() require("avante.api").edit() end, desc = "avante: edit", mode = "v" },
-  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1" -- for windows
   dependencies = {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
@@ -32,6 +32,14 @@ return {
           use_absolute_path = true,
         },
       },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
     },
   },
 }
